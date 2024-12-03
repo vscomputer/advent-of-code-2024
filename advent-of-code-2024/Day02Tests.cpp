0f53a5c72c2c0e03479573f4aaad1120234f0abe
Clint Hoagland <c.hoagland@techsmith.com>
2024-12-03 16:52:57 -0500

    +Day02 part 2

--- advent-of-code-2024/Day02Tests.cpp
+++ advent-of-code-2024/Day02Tests.cpp
@@ -6,6 +6,25 @@
 #include <regex>
 #include <cstdio> 
 
+class UnsafeReport
+{
+public:
+	UnsafeReport();
+	~UnsafeReport();
+	std::vector<int> Report;
+	int CurrentWhenRejected;
+private:
+
+};
+
+UnsafeReport::UnsafeReport()
+{
+}
+
+UnsafeReport::~UnsafeReport()
+{
+}
+
 class DetectsSafeReports
 {
 public:
@@ -15,8 +34,11 @@ public:
 	int GetNumberOfReports();
 	std::vector<std::vector<int>> Reports;
 	bool ReportIsSafe(int reportNumber);
+	bool ReportIsSafe(std::vector<int> report, bool collectUnsafeReports);
+	std::vector<UnsafeReport> UnsafeReports;
+	bool UnsafeReportIsTolerable(int unsafeReportNumber);
 private:
-	
+	void CollectUnsafeReport(std::vector<int> report, int current);
 };
 
 DetectsSafeReports::DetectsSafeReports()
@@ -58,37 +80,73 @@ int DetectsSafeReports::GetNumberOfReports()
 	return Reports.size();
 }
 
+
 bool DetectsSafeReports::ReportIsSafe(int reportNumber)
 {
-	std::vector<int> report = Reports[reportNumber];
+	return ReportIsSafe(Reports[reportNumber], true);	
+}
 
-	/*for (int i = 0; i < report.size(); i++)
-	{
-		std::cout << report[i] << " ";
-	}*/
-
+bool DetectsSafeReports::ReportIsSafe(std::vector<int> report, bool collectUnsafeReports = true)
+{
 	int current = -1;
 	int next = -1;
 	bool descending = false;
 
 	for (int i = 0; i < report.size() - 1; i++)
-	{		
+	{
 		current = report[i];
 		next = report[i + 1];
-		if (i == 0 )
+		if (i == 0)
 		{
 			descending = current > next;
 		}
 		else if ((descending && next > current) || (!descending && next < current))
 		{
+			if(collectUnsafeReports)
+				CollectUnsafeReport(report, i);
 			return false;
 		}
-		
+
 		if (abs(current - next) > 3 || abs(current - next) == 0)
+		{
+			if (collectUnsafeReports)
+				CollectUnsafeReport(report, i);
 			return false;
+		}
 	}
-	
+
 	return true;
+}
+
+bool DetectsSafeReports::UnsafeReportIsTolerable(int unsafeReportNumber)
+{	
+	if (UnsafeReports[unsafeReportNumber].CurrentWhenRejected >= 1)
+	{
+		std::vector<int> leftCandidate = UnsafeReports[unsafeReportNumber].Report;
+		leftCandidate.erase(leftCandidate.begin() + UnsafeReports[unsafeReportNumber].CurrentWhenRejected - 1);
+		if (ReportIsSafe(leftCandidate, false))
+			return true;
+	}
+
+	std::vector<int> centerCandidate = UnsafeReports[unsafeReportNumber].Report;
+	centerCandidate.erase(centerCandidate.begin() + UnsafeReports[unsafeReportNumber].CurrentWhenRejected);
+	if (ReportIsSafe(centerCandidate, false))
+		return true;
+
+	std::vector<int> rightCandidate = UnsafeReports[unsafeReportNumber].Report;
+	rightCandidate.erase(rightCandidate.begin() + UnsafeReports[unsafeReportNumber].CurrentWhenRejected + 1);
+	if (ReportIsSafe(rightCandidate, false))
+		return true;
+	
+	return false;
+}
+
+void DetectsSafeReports::CollectUnsafeReport(std::vector<int> report, int current)
+{
+	UnsafeReport unsafeReport;
+	unsafeReport.Report = report;
+	unsafeReport.CurrentWhenRejected = current;
+	UnsafeReports.push_back(unsafeReport);
 }
 
 TEST(DetectsSafeReports, LoadReports_TestFile_CorrectNumberOfReports)
@@ -138,9 +196,68 @@ TEST(DetectsSafeReports, DISABLED_ReportIsSafe_ActualData_CorrectReports)
 		bool safe = subject.ReportIsSafe(i);
 
 		if (safe)
-			result++;
-		//std::cout << " " << safe << std::endl;
+			result++;		
 	}
 
-	EXPECT_EQ(result, 0);
+	EXPECT_EQ(result, -1);
+}
+
+TEST(DetectsSafeReports, DISABLED_ReportIsSafe_ActualData_CorrectUnsafeReports)
+{
+	DetectsSafeReports subject;
+
+	subject.LoadReports("C:\\Projects\\Git\\advent-of-code-2024-inputs\\day-2-actual-real.txt");
+	
+	for (int i = 0; i < subject.GetNumberOfReports(); i++)
+	{
+		subject.ReportIsSafe(i);				
+	}
+
+	EXPECT_EQ(subject.UnsafeReports.size(), -1);
+}
+
+TEST(DetectsSafeReports, UnsafeReportIsTolerable_SampleData_CorrectReports)
+{
+	DetectsSafeReports subject;
+
+	subject.LoadReports("C:\\Projects\\Git\\advent-of-code-2024-inputs\\day-2-test.txt");
+	for (int i = 0; i < subject.GetNumberOfReports(); i++)
+	{
+		subject.ReportIsSafe(i);
+	}
+
+	EXPECT_FALSE(subject.UnsafeReportIsTolerable(0)); //Unsafe regardless of which level is removed.
+	EXPECT_FALSE(subject.UnsafeReportIsTolerable(1)); //Unsafe regardless of which level is removed.
+	EXPECT_TRUE(subject.UnsafeReportIsTolerable(2)); //Safe by removing the second level, 3.
+	EXPECT_TRUE(subject.UnsafeReportIsTolerable(3)); //Safe by removing the third level, 4.
+}
+
+TEST(DetectsSafeReports, DISABLED_ReportIsSafe_ActualData_CorrectReportsIncludingTolerable)
+{
+	DetectsSafeReports subject;
+
+	subject.LoadReports("C:\\Projects\\Git\\advent-of-code-2024-inputs\\day-2-actual-real.txt");
+
+	int safeResult = 0;
+
+	for (int i = 0; i < subject.GetNumberOfReports(); i++)
+	{
+		bool safe = subject.ReportIsSafe(i);
+
+		if (safe)
+			safeResult++;
+	}
+
+	int tolerableResult = 0;
+	for (int i = 0; i < subject.UnsafeReports.size(); i++)
+	{
+		bool tolerable = subject.UnsafeReportIsTolerable(i);
+
+			if (tolerable)
+				tolerableResult++;
+	}
+	
+	int result = safeResult + tolerableResult;
+	
+	EXPECT_EQ(result, -1);
 }
